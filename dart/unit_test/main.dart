24caa37f5abf08e37615c400c6fd5b9d93c15db3
jt <jakezj.tan@gmail.com>
2023-03-11 15:27:24 +0800

    modified main again

--- dart/unit_test/main.dart
+++ dart/unit_test/main.dart
@@ -1,4 +1,4 @@
-import 'package:test/test.dart'
+import 'package:test/test.dart';
 
 int getSumOfArray(List<int> arr){
 	int res = 0;
@@ -12,6 +12,6 @@ void main(){
 	test('test for getSumOfArray', (){
 		int expectedValue = 15;
 		int actualValue = getSumOfArray([1, 2, 3, 4, 5]);
-		expect(expectValue, actualValue);
+		expect(expectedValue, actualValue);
 	});
 }
